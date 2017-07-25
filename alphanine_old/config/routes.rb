61c99ba85905e60cff235c8f30cc5195505bfda3
rodscotolo <rodolfoscotolo@gmail.com>
2017-07-24 21:57:23 -0300

    remove files

--- alphanine_old/config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  mount Ckeditor::Engine => '/ckeditor'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "posts#index"
-
-  resources :posts
-end
